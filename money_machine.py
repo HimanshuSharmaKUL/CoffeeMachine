c0e3daee34cd77babb05d83e29fcb48c05d61417
himanshu <himanshu.sharma@student.kuleuven.be>
2024-03-20 23:54:22 +0100

    Updates

--- money_machine.py
+++ money_machine.py
@@ -20,3 +20,22 @@ class MoneyMachine:
         """Prints the current profit"""
         print(f"Money: {self.CURRENCY}{self.profit}")
     
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins")
+        for coin in self.COIN_VALUES:
+            self.money_received += int(input(f"How many {coin}?: "))*self.COIN_VALUES[coin]
+        return self.money_received
+    
+    def make_payment(self, cost):
+        """Returns true when payment is accepted, or False if insufficient."""
+        self.process_coins()
+        if self.money_recieved >= cost:
+            change = round(self.money_recieved-cost, 2)
+            print(f"Here is {self.CURRENCY}{change} in change.")
+            self.profit += cost
+            return True
+        else:
+            print("sorry that's not enough money. Money refunded.")
+            self.money_recieved = 0
+            return False
